04970d86ede5bb296a05b902b122437fbd7d2233
Miraç ERDİN <mirac.erdin@pusulayazilim.com.tr>
2026-05-21 12:24:21 +0300

    update API URL to production server

--- constants/Config.ts
+++ constants/Config.ts
@@ -3,7 +3,7 @@
 // For Android Emulator, use 'http://10.0.2.2:3000'
 // For iOS Simulator, 'http://localhost:3000' works using localhost
 
-const API_URL = "http://192.168.1.2:3001";
+const API_URL = "https://linkflow-server-uask.onrender.com";
 
 export default {
   API_URL,
