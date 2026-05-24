b258573b724c3b070d0bde64f70a9cbd63e7d311
Miraç ERDİN <mirac.erdin@pusulayazilim.com.tr>
2026-05-24 19:30:14 +0300

    fix: use environment variable for api url

--- constants/Config.ts
+++ constants/Config.ts
@@ -3,7 +3,7 @@
 // For Android Emulator, use 'http://10.0.2.2:3000'
 // For iOS Simulator, 'http://localhost:3000' works using localhost
 
-const API_URL = "https://linkflow-server-uask.onrender.com";
+const API_URL = process.env.EXPO_PUBLIC_API_URL || "https://linkflow-server-uask.onrender.com";
 
 export default {
   API_URL,
